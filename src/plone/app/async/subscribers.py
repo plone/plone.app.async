ce7a979e48319269ebf24411848f459b097afcae
Stefan H. Holek <stefan@epy.co.at>
2011-07-21 14:29:25 +0000

    Increase dispatcher ping intervals to avoid ZODB bloat.
    svn path=/plone.app.async/trunk/; revision=51404

--- src/plone/app/async/subscribers.py
+++ src/plone/app/async/subscribers.py
@@ -1,18 +1,32 @@
 import logging
+import datetime
 from zope.event import notify
 from plone.app.async.interfaces import QueueReady
 
 logger = logging.getLogger('plone.app.async')
 
 
+def set_ping_intervals(dispatcher, ping_secs, ping_death_secs):
+    """Set ping intervals."""
+    dispatcher.ping_interval = datetime.timedelta(seconds=ping_secs)
+    dispatcher.ping_death_interval = datetime.timedelta(seconds=ping_death_secs)
+    logger.info('dispatcher ping intervals configured')
+
+
 def set_quota(queue, quota_name, size):
     """Create quota or modify existing."""
     if quota_name in queue.quotas:
         queue.quotas[quota_name].size = size
-        logger.info("Configured quota %r in queue %r", quota_name, queue.name)
+        logger.info("quota %r configured in queue %r", quota_name, queue.name)
     else:
         queue.quotas.create(quota_name, size=size)
-        logger.info("Quota %r added to queue %r", quota_name, queue.name)
+        logger.info("quota %r added to queue %r", quota_name, queue.name)
+
+
+def configureDispatcher(event):
+    """Subscriber for IDispatcherActivated."""
+    dispatcher = event.object
+    set_ping_intervals(dispatcher, 15*60, 30*60)
 
 
 def notifyQueueReady(event):
