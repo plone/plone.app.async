f14bdec0b44610081b0112c6a8696a510be55e6c
David Glick <dglick@gmail.com>
2012-01-24 01:02:58 -0800

    refactor to render job info in js based on JSON fetched periodically

--- src/plone/app/async/browser/queue.py
+++ src/plone/app/async/browser/queue.py
@@ -1,5 +1,6 @@
 from DateTime import DateTime
 from datetime import datetime
+from zope.cachedescriptors.property import Lazy as lazy_property
 from zope.component import getUtility
 from Products.Five import BrowserView
 from zc.async.interfaces import ACTIVE, COMPLETED
@@ -8,16 +9,12 @@ from zc.twist import Failure
 from plone.app.async.interfaces import IAsyncService
 from webdav.xmltools import escape
 from ZODB.utils import p64, u64
+import simplejson as json
 import pytz
 
 
 local_zone = DateTime().asdatetime().tzinfo
 
-
-def filter_jobs(jobs, portal_path):
-    for job in jobs:
-        if getattr(job, 'portal_path', None) == portal_path:
-            yield job
 
 def get_failure(job):
     if job.status == COMPLETED and isinstance(job.result, Failure):
@@ -26,33 +23,94 @@ def get_failure(job):
         return job._retry_policy.data['last_job_error']
 
 
-class QueueView(BrowserView):
+class JobsView(BrowserView):
 
-    def __call__(self):
-        portal_path = self.context.getPhysicalPath()
-        self.now = datetime.now(pytz.UTC)
-        self.queued_jobs = []
-        self.active_jobs = []
-        self.completed_jobs = []
-        self.dead_jobs = []
+    js = """
+jQuery(function($) {
+  var update = function() {
+    $.fn.render = function(data) {
+      var rows = ['<tr><th>Job</th><th>Status</th></tr>'];
+      $(data).each(function(i, job) {
+        row = ['<tr><td><div><strong>' + job.callable + '</strong></div>'];
+        row.push('<div>' + job.args + '</div></td>');
+        row.push('<td>' + job.status);
+        if (job.progress)
+          row.push('<div>' + job.progress + '</div>');
+        if (job.failure)
+          row.push('<div>' + job.failure + '</div>')
+        rows.push(row.join('') + '</tr>');
+      });
+      $('table', this).html(rows.join(''));
+      var form = this.closest('form');
+      var legend = $('legend', this);
+      $('.formTab span', form).eq($('legend', form).index(legend)).html(legend.html().replace('0', data.length));
+    };
+  
+    $.getJSON('jobs.json', function(data) {
+      $('#queued-jobs').render(data.queued);
+      $('#active-jobs').render(data.active);
+      $('#dead-jobs').render(data.dead);
+      $('#completed-jobs').render(data.completed);
+    });
 
+    setTimeout(update, 5000);
+  };
+  update();
+});
+"""
+
+
+class JobsJSON(BrowserView):
+
+    def _find_jobs(self):
         service = getUtility(IAsyncService)
         queue = service.getQueues()['']
-        for job in filter_jobs(queue, portal_path):
-            self.queued_jobs.append(job)
+        for job in queue:
+            yield 'queued', job
         for da in queue.dispatchers.values():
             for agent in da.values():
-                for job in filter_jobs(agent, portal_path):
-                    self.active_jobs.append(job)
-                for job in filter_jobs(agent.completed, portal_path):
+                for job in agent:
+                    yield 'active', job
+                for job in agent.completed:
                     if isinstance(job.result, Failure):
-                        self.dead_jobs.append(job)
+                        yield 'dead', job
                     else:
-                        self.completed_jobs.append(job)
+                        yield 'completed', job
+    
+    def _filter_jobs(self):
+        for job_status, job in self._find_jobs():
+            if getattr(job, 'portal_path', None) == self.portal_path:
+                yield job_status, job
 
-        return self.index()
+    @lazy_property
+    def portal_path(self):
+        return self.context.getPhysicalPath()
+    
+    @lazy_property
+    def now(self):
+        return datetime.now(pytz.UTC)
 
-    def format_timing(self, job):
+    def __call__(self):
+        jobs = {
+            'queued': [],
+            'active': [],
+            'completed': [],
+            'dead': [],
+        }
+
+        for job_status, job in self._filter_jobs():
+            jobs[job_status].append({
+                'id': u64(job._p_oid),
+                'callable': custom_repr(job.callable),
+                'args': self.format_args(job),
+                'status': self.format_status(job),
+                'progress': self.format_progress(job),
+                'failure': self.format_failure(job),
+            })
+
+        return json.dumps(jobs)
+
+    def format_status(self, job):
         if job.status == COMPLETED:
             return 'Completed at %s' % self.format_datetime(job.active_end)
         elif job.status == ACTIVE:
@@ -77,7 +135,7 @@ class QueueView(BrowserView):
         if not progress:
             return ''
 
-        return '<div style="width:100px; border: solid 1px #000;"><div style="width:%dpx; background: red;">&nbsp;</div></div>%0.2f%%' % (progress, progress)
+        return '<div style="width:100px; border: solid 1px #000;"><div style="width:%dpx; background: red;">&nbsp;</div></div>%d%%' % (progress, progress)
 
     def format_args(self, job):
         args = ', '.join(custom_repr(a) for a in job.args)
@@ -86,7 +144,7 @@ class QueueView(BrowserView):
             args += ", " + kwargs
         elif kwargs:
             args = kwargs
-        return args
+        return 'Args: %s' % args
 
     def format_failure(self, job):
         failure = get_failure(job)
@@ -100,9 +158,6 @@ class QueueView(BrowserView):
     def format_datetime(self, dt):
         return dt.astimezone(local_zone).strftime('%I:%M:%S %p, %Y-%m-%d')
 
-    def custom_repr(self, ob):
-        return custom_repr(ob)
-
 
 class TracebackView(BrowserView):
 
@@ -112,9 +167,6 @@ class TracebackView(BrowserView):
         failure = get_failure(job)
         return escape(failure.getTraceback())
 
-
-# Need to show:
-# - ideally, progress bar based on job annotations
 
 import time
 from plone.app.async import Job, queue, RetryWithDelay
